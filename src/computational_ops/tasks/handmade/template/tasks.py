073b7d276ad64912cfff7061cd77687676871f0c
Shimataka <40866638+Shimataka@users.noreply.github.com>
2024-12-16 00:10:41 +0900

    feat: enhance TemplateTask with YAML validation and asynchronous execution
    - Updated the TemplateTask class to accept a new parameter `from_yaml` in the constructor for improved task initialization.
    - Implemented a `validate_yaml` method to validate YAML instructions, returning appropriate results for error handling.
    - Modified the `run` method to utilize the validated `from_yaml` and handle asynchronous execution with improved error management.
    - Introduced helper functions `some_check` and `some_function` to encapsulate validation and task execution logic, enhancing code organization and readability.

--- src/computational_ops/tasks/handmade/template/tasks.py
+++ src/computational_ops/tasks/handmade/template/tasks.py
@@ -1,16 +1,41 @@
 from computational_ops.core.instruction import Instruction
-from computational_ops.core.result import Ok, Result
+from computational_ops.core.result import Err, Ok, Result
 from computational_ops.core.task import Task, TaskOutput
 
 
 class TemplateTask(Task):
-    def __init__(self) -> None:
-        super().__init__(name="TemplateTask")
+    def __init__(self, from_yaml: Instruction) -> None:
+        super().__init__(name="TemplateTask", from_yaml=from_yaml)
+
+    def validate_yaml(self, from_yaml: Instruction) -> Result[Instruction, str]:
+        result = some_check(from_yaml)
+        if result.is_err():
+            return Err(result.unwrap_err())
+        return Ok(from_yaml)
 
     async def run(
         self,
-        from_yaml: Instruction,
         from_pedecessor: TaskOutput,
     ) -> Result[TaskOutput, str]:
         print("TemplateTask is running")
-        return Ok(TaskOutput(data={"template_task": "TemplateTask is running"}))
+        result = await some_function(self.from_yaml, from_pedecessor)
+        if result.is_err():
+            return Err(result.unwrap_err())
+        print("TemplateTask is done")
+        return Ok(result.unwrap())
+
+
+def some_check(from_yaml: Instruction) -> Result[Instruction, str]:
+    print("some_check is running")
+    print(from_yaml)
+    return Ok(from_yaml)
+
+
+async def some_function(
+    from_yaml: Instruction,
+    from_pedecessor: TaskOutput,
+) -> Result[TaskOutput, str]:
+    print("Now, I'm doing something")
+    print(from_yaml)
+    print(from_pedecessor)
+    return Ok(TaskOutput(data={"template_task": "TemplateTask is running"}))
